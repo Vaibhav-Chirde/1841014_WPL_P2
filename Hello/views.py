84abc13e798aa1abdae6a71b882850ef06f50f8f
vaibhav_chirde <vrchirde@gmail.com>
2021-04-06 17:41:49 +0530

    first commit

---
+++ Hello/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def show(request):
+    return HttpResponse("Hello , This is Vaibhav from TY Computer and My PRN is 1841014")
